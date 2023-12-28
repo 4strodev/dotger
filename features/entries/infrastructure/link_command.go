cc0ec56fdc0d1cc4e81c512034dfd7c22d1e8eb8
4strodev <jumarurex@gmail.com>
2023-12-28 21:40:48 +0100

    chore:add comments

--- features/entries/infrastructure/link_command.go
+++ features/entries/infrastructure/link_command.go
@@ -3,7 +3,6 @@ package infrastructure
 import (
 	"errors"
 	"fmt"
-	"os"
 	"path/filepath"
 
 	"github.com/4strodev/dotger/features/entries/actions"
@@ -16,39 +15,46 @@ import (
 )
 
 func GetLinkCommand(inject injector.Injector) *cli.Command {
-	return &cli.Command {
-		Name: "link",
-		Usage: "link a dotger entry to their destination",
+	return &cli.Command{
+		Name:      "link",
+		ArgsUsage: "<entry path>",
+		Usage:     "link a dotger entry to their destination",
+		Aliases:   []string{"l"},
 		Action: func(ctx *cli.Context) error {
 			const configFile = ".dotger.toml"
+			// Checcking if command line arguments are correct
 			args := ctx.Args()
 			if args.Len() != 1 {
-				return errors.New("Expected 1 argument")
+				return errors.New("Expected <entry path> argument")
 			}
 
-			entryName := args.First()
-			exists, err := inject.FileSystem.Exists(entryName)
+			// Getting entry name from command line arguments
+			entryPath := args.First()
+			exists, err := inject.FileSystem.Exists(entryPath)
 			if err != nil {
 				return err
 			}
 			if !exists {
-				return fmt.Errorf("entry '%s' doesn't exists", entryName)
+				return fmt.Errorf("No dotger entry found at '%s'", entryPath)
 			}
 
-			dotgerFile := filepath.Join(entryName, configFile)
+			// Getting full path of dotfer file
+			dotgerFile := filepath.Join(entryPath, configFile)
 			exists, err = inject.FileSystem.Exists(dotgerFile)
 			if err != nil {
 				return err
 			}
 			if !exists {
-				return fmt.Errorf("entry '%s' doesn't have '%s' file", entryName, configFile)
+				return fmt.Errorf("No '%s' file found at '%s'", configFile, entryPath)
 			}
 
+			// Reading config file
 			content, err := inject.FileSystem.ReadFile(dotgerFile)
 			if err != nil {
 				return err
 			}
 
+			// Unmarshalling config file
 			var entryConfig domain.EntryConfig
 			k := koanf.New(".")
 			k.Load(rawbytes.Provider(content), toml.Parser())
@@ -57,11 +63,8 @@ func GetLinkCommand(inject injector.Injector) *cli.Command {
 				return err
 			}
 
-			wd, err := os.Getwd()
-			if err != nil {
-				return err
-			}
-			err = actions.LinkEntry(inject, ctx.Context, filepath.Join(wd, entryName), entryConfig)
+			// Getting current working directory
+			err = actions.LinkEntry(inject, ctx.Context, entryPath, entryConfig)
 			if err != nil {
 				return err
 			}
